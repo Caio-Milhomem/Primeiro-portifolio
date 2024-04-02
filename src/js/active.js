2a97dbc9c152ae94a6771dc642e1bfbd4d54db4a
Caio-Milhomem <caiomilhomem@outlook.com.br>
2024-04-02 10:51:59 -0300

    add home to active

--- src/js/active.js
+++ src/js/active.js
@@ -6,7 +6,7 @@ const navLinks = document.querySelectorAll('.nav-link');
 // Function to check which section is in view
 function updateActiveSection() {
   sections.forEach(section => {
-    const sectionTop = section.offsetTop - 50; // Adjust the offset as needed
+    const sectionTop = section.offsetTop - 300; // Adjust the offset as needed
     const sectionHeight = section.clientHeight;
     if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
       // Remove the "active" class from all links
@@ -19,13 +19,7 @@ function updateActiveSection() {
     }
   });
 
-  // Check if scrolling through the header section
-  const header = document.getElementById('header');
-  if (window.scrollY <= header.clientHeight) {
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-    });
-  }
+
 }
 
 // Add scroll event listener to update active section on scroll
